6043d99e7adace0d8a506625e459c1e4739b010c
scottdharris <scottdharris11@yahoo.com>
2023-12-01 16:42:39 -0600

    day 2 starter

---
+++ day02.py
@@ -0,0 +1,30 @@
+from utilities.data import read_lines
+from utilities.runner import Runner
+
+@Runner("Day 2", "Part 1")
+def solve_part1(lines: list):
+    return -1
+
+@Runner("Day 2", "Part 2")
+def solve_part2(lines: list):
+    return -1
+
+# Part 1
+part1_sample = [
+    
+]
+
+value = solve_part1(part1_sample)
+assert(value == 0)
+value = solve_part1(read_lines("input/day2-input.txt"))
+assert(value == 0)
+
+# Part 2
+part2_sample = [
+    
+]
+
+value = solve_part2(part2_sample)
+assert(value == 0)
+value = solve_part2(read_lines("input/day2-input.txt"))
+assert(value == 0)
