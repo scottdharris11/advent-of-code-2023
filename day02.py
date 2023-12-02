190855e10f50de46d717d5b35fa4ecd2f09e476e
scottdharris <scottdharris11@yahoo.com>
2023-12-01 23:40:44 -0600

    day 2 solution

--- day02.py
+++ day02.py
@@ -3,28 +3,74 @@ from utilities.runner import Runner
 
 @Runner("Day 2", "Part 1")
 def solve_part1(lines: list):
-    return -1
+    actual = {"red":12, "green": 13, "blue": 14}
+    total = 0
+    for line in lines:
+        game = parse_game(line)
+        if game.possible(actual):
+            total += game.id
+    return total
 
 @Runner("Day 2", "Part 2")
 def solve_part2(lines: list):
-    return -1
+    total = 0
+    for line in lines:
+        game = parse_game(line)
+        total += game.power()
+    return total
 
-# Part 1
-part1_sample = [
+def parse_game(line: str):
+    idStart = line.index(" ")
+    idEnd = line.index(":")
+    id = int(line[idStart+1:idEnd])
+    game = Game(id)
     
+    sets = line[idEnd+1:].split(";")
+    for set in sets:
+        cubes = set.split(",")
+        for cube in cubes:
+            c = cube.strip().split(" ")
+            game.cubesShown(c[1], int(c[0]))
+    
+    return game
+    
+class Game:
+    def __init__(self, id) -> None:
+        self.id = id
+        self.max_cubes = {}
+    
+    def cubesShown(self, color, count):
+        if self.max_cubes.get(color, 0) < count:
+            self.max_cubes[color] = count
+    
+    def possible(self, cubes: dict):
+        for cube in cubes.keys():
+            if self.max_cubes[cube] > cubes[cube]:
+                return False
+        return True
+    
+    def power(self):
+        p = 1
+        for cube in self.max_cubes:
+            p *= self.max_cubes[cube]
+        return p
+    
+# Part 1
+sample = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
 ]
 
-value = solve_part1(part1_sample)
-assert(value == -1)
+value = solve_part1(sample)
+assert(value == 8)
 value = solve_part1(read_lines("input/day2-input.txt"))
-assert(value == -1)
+assert(value == 2795)
 
 # Part 2
-part2_sample = [
-    
-]
-
-value = solve_part2(part2_sample)
-assert(value == -1)
+value = solve_part2(sample)
+assert(value == 2286)
 value = solve_part2(read_lines("input/day2-input.txt"))
-assert(value == -1)
+assert(value == 75561)
