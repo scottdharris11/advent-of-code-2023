abfce9995a265b1196fa99bcfd7b6edb1cea9a11
scottdharris <scottdharris11@yahoo.com>
2023-12-04 21:00:15 -0600

    day 5 template

---
+++ day05.py
@@ -0,0 +1,27 @@
+from utilities.data import read_lines
+from utilities.runner import Runner
+
+@Runner("Day 5", "Part 1")
+def solve_part1(lines: list):
+    return -1
+
+@Runner("Day 5", "Part 2")
+def solve_part2(lines: list):
+    return -1
+
+# Part 1
+input = read_lines("input/day5-input.txt")
+sample = [
+    
+]
+
+value = solve_part1(sample)
+assert(value == -1)
+value = solve_part1(input)
+assert(value == -1)
+
+# Part 2
+value = solve_part2(sample)
+assert(value == -1)
+value = solve_part2(input)
+assert(value == -1)
