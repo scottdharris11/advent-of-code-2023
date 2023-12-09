7d0d6b64cbf7958e365d8137daf3c075f045033b
scottdharris <scottdharris11@yahoo.com>
2023-12-08 23:50:49 -0600

    day 9 part 2

--- day09.py
+++ day09.py
@@ -10,7 +10,10 @@ def solve_part1(lines: list):
 
 @Runner("Day 9", "Part 2")
 def solve_part2(lines: list):
-    return -1
+    total = 0
+    for line in lines:
+        total += prev_value(parse_integers(line, " "))
+    return total
 
 def next_value(values: list[int]) -> int:
     diffs = []
@@ -23,7 +26,18 @@ def next_value(values: list[int]) -> int:
     if allzeroes:
         return 0
     return values[-1] + next_value(diffs)
-        
+
+def prev_value(values: list[int]) -> int:
+    diffs = []
+    allzeroes = True
+    for i in range(len(values)-1):
+        d = values[i+1] - values[i]
+        diffs.append(d)
+        if values[i+1] != 0 or values[i] != 0:
+            allzeroes = False
+    if allzeroes:
+        return 0
+    return values[0] - prev_value(diffs)      
 
 # Part 1
 input = read_lines("input/day9-input.txt")
@@ -36,6 +50,6 @@ assert(value == 1696140818)
 
 # Part 2
 value = solve_part2(sample)
-assert(value == -1)
+assert(value == 2)
 value = solve_part2(input)
-assert(value == -1)
+assert(value == 1152)
