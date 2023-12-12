d8752e25664be109de888fbb000cfe177a755eeb
scottdharris <scottdharris11@yahoo.com>
2023-12-12 15:23:16 -0500

    day 12 part 1, faster but still wrong

--- day12.py
+++ day12.py
@@ -6,33 +6,43 @@ def solve_part1(lines: list):
     total = 0
     for line in lines:
         s = line.split(" ")
-        total += possibilities(s[0], parse_integers(s[1], ","))
+        total += possibilities(Record(s[0], parse_integers(s[1], ",")))
     return total
 
 @Runner("Day 12", "Part 2")
 def solve_part2(lines: list):
     return -1
 
-def possibilities(mask: str, pattern: list[int]) -> int:
-    unknown = mask.count('?')
-    tofill = sum(pattern) - mask.count('#')
+class Record:
+    def __init__(self, mask: str, pattern: list[int]) -> None:
+        self.mask = mask
+        self.pattern = pattern
+    
+    def __repr__(self):
+        return str((self.mask, self.pattern))
+
+def possibilities(r: Record) -> int:
+    #print(r)
+    unknown = r.mask.count('?')
+    tofill = sum(r.pattern) - r.mask.count('#')
+    if tofill == 0:
+        return 0
     placements = []
     for i in range(unknown-tofill+1):
-        fill(mask, tofill, unknown, tuple(), i, placements)
-    p = 0
-    for placement in placements:
-        if possible(mask, placement, pattern):
-            p += 1
-    return p
+        fill(r, tofill, unknown, tuple(), i, placements)
+    #print(placements)
+    return len(placements)
 
-def fill(mask: str, tofill: int, unknown: int, filled: tuple[int], start: int, placements: list):
+def fill(r: Record, tofill: int, unknown: int, filled: tuple[int], start: int, placements: list):
     f = list(filled)
-    f.append(unknownStrIndex(mask, start))
+    f.append(unknownStrIndex(r.mask, start))
+    if not possible(r, f):
+        return
     if len(f) == tofill:
         placements.append(tuple(f))
         return
     for i in range(start+1, unknown):
-        fill(mask, tofill, unknown, tuple(f), i, placements)
+        fill(r, tofill, unknown, tuple(f), i, placements)
         
 def unknownStrIndex(mask: str, i: int) -> int:
     count = 0
@@ -42,26 +52,30 @@ def unknownStrIndex(mask: str, i: int) -> int:
                 return si
             count += 1
 
-def possible(mask: str, unknown: tuple[int], pattern: list[int]) -> bool:
+def possible(r: Record, placement: tuple[int]) -> bool:
     uCnt = 0
     uIdx = 0
-    for i, c in enumerate(mask):
+    placed = 0
+    for i, c in enumerate(r.mask):
         if c == '?':
+            if placed == len(placement):
+                return uCnt <= r.pattern[uIdx]
             c = "."
-            if i in unknown:
+            if i in placement:
+                placed += 1
                 c = "#"
         if c == "#":
             uCnt += 1
         else:
             if uCnt == 0:
                 continue
-            if uCnt != pattern[uIdx]:
+            if uCnt != r.pattern[uIdx]:
                 return False
             uIdx += 1
             uCnt = 0
-            if uIdx == len(pattern):
+            if placed == len(placement):
                 return True
-    return uCnt == pattern[-1]
+    return uCnt == r.pattern[-1]
             
 
 # Part 1
@@ -70,8 +84,10 @@ sample = read_lines("input/day12-sample.txt")
 
 value = solve_part1(sample)
 assert(value == 21)
+#value = solve_part1(["#..??####??? 1,4"])
 value = solve_part1(input)
 assert(value > 7218)
+assert(value < 7851)
 
 # Part 2
 value = solve_part2(sample)
