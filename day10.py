7eff1eff58dbc1e8122bee42a1248d97c269a026
scottdharris <scottdharris11@yahoo.com>
2023-12-15 23:10:18 -0500

    day 10 part 2 solution

--- day10.py
+++ day10.py
@@ -11,28 +11,14 @@ def solve_part1(lines: list):
 def solve_part2(lines: list):
     pipes, start = input_to_pipes(lines)
     navigate_pipe(pipes, start)
-    outside = set()
-    for y, row in enumerate(pipes):
-        for x, col in enumerate(row):
-            if col == None or not col.inloop:
-                pos = (x, y)
-                if pos not in outside:
-                    es = EdgeSearcher(pos, pipes, outside)
-                    s = Search(es)
-                    solution = s.best(SearchMove(0, pos))
-                    if solution == None:
-                        continue
-                    outside = outside.union(solution.path)
-
-    inside = 0
-    for y, row in enumerate(pipes):
-        for x, col in enumerate(row):
-            if col == None or not col.inloop:
-                pos = (x, y)
-                if pos not in outside:
-                    print(pos)
-                    inside += 1
-    return inside
+    maze = PipeMaze(pipes)
+    inside = set()
+    for y in range(maze.row_count):
+        for x in range(maze.col_count):
+            pos = (x, y)
+            if maze.inside(pos):
+                inside.add(pos)
+    return len(inside)
 
 VERTICAL = '|'
 HORIZONTAL = "-"
@@ -73,25 +59,107 @@ class Pipe:
             self.links.append(p.pos)
             p.links.append(self.pos)
 
-class EdgeSearcher(Searcher):
-    def __init__(self, point: tuple[int], pipes: list[list[Pipe]], outside: list[tuple]) -> None:
-        self.point = point
+class PipeMaze:
+    def __init__(self, pipes: list[list[Pipe]]) -> None:
         self.pipes = pipes
-        self.rows = len(pipes)
-        self.cols = len(pipes[0])
-        self.outside = outside
-        
-    def is_goal(self, obj) -> bool:
-        if obj in self.outside:
-            return True
-        if obj[0] < 0 or obj[0] == self.cols:
-            return True
-        if obj[1] < 0 or obj[1] == self.rows:
-            return True
-        return False
+        self.row_count = len(pipes)
+        self.col_count = len(pipes[0])
     
+    def inside(self, point: tuple[int]) -> bool:
+        if self.__pipe(point[0], point[1]) != None:
+            return False
+        bottom = self.vert_pipes_to_outside(point, 1)
+        top = self.vert_pipes_to_outside(point, -1)
+        right = self.horz_pipes_to_outside(point, 1)
+        left = self.horz_pipes_to_outside(point, -1)
+        if bottom == 0 or top == 0 or right == 0 or left == 0:
+            return False
+        if bottom % 2 == 0 or top % 2 == 0 or right % 2 == 0 or left % 2 == 0:
+            return False
+        return True
+    
+    def vert_pipes_to_outside(self, point: tuple[int], dir: int) -> int:
+        crossed = 0
+        y = point[1] + dir
+        elbow = ""
+        while y >= 0 and y < self.row_count:
+            p = self.__pipe(point[0], y)
+            y += dir
+            if p == None or p.type == VERTICAL:
+                continue
+            crossed += 1
+            if p.type == HORIZONTAL:
+                continue
+            if p.type == NORTH_TO_EAST and elbow == SOUTH_TO_EAST:
+                crossed -= 2
+                elbow = ""
+            elif p.type == SOUTH_TO_EAST and elbow == NORTH_TO_EAST:
+                crossed -= 2
+                elbow = ""
+            elif p.type == NORTH_TO_EAST and elbow == SOUTH_TO_WEST:
+                crossed -= 1
+                elbow = ""
+            elif p.type == SOUTH_TO_EAST and elbow == NORTH_TO_WEST:
+                crossed -= 1
+                elbow = ""
+            elif p.type == NORTH_TO_WEST and elbow == SOUTH_TO_WEST:
+                crossed -= 2
+                elbow = ""
+            elif p.type == SOUTH_TO_WEST and elbow == NORTH_TO_WEST:
+                crossed -= 2
+                elbow = ""
+            elif p.type == NORTH_TO_WEST and elbow == SOUTH_TO_EAST:
+                crossed -= 1
+                elbow = ""
+            elif p.type == SOUTH_TO_WEST and elbow == NORTH_TO_EAST:
+                crossed -= 1
+                elbow = ""
+            else:
+                elbow = p.type
+        return crossed
+    
+    def horz_pipes_to_outside(self, point: tuple[int], dir: int) -> int:
+        crossed = 0
+        x = point[0] + dir
+        elbow = ""
+        while x >= 0 and x < self.col_count:
+            p = self.__pipe(x, point[1])
+            x += dir
+            if p == None or p.type == HORIZONTAL:
+                continue
+            crossed += 1
+            if p.type == VERTICAL:
+                continue
+            if p.type == NORTH_TO_EAST and elbow == NORTH_TO_WEST:
+                crossed -= 2
+                elbow = ""
+            elif p.type == SOUTH_TO_EAST and elbow == SOUTH_TO_WEST:
+                crossed -= 2
+                elbow = ""
+            elif p.type == NORTH_TO_EAST and elbow == SOUTH_TO_WEST:
+                crossed -= 1
+                elbow = ""
+            elif p.type == SOUTH_TO_EAST and elbow == NORTH_TO_WEST:
+                crossed -= 1
+                elbow = ""
+            elif p.type == NORTH_TO_WEST and elbow == NORTH_TO_EAST:
+                crossed -= 2
+                elbow = ""
+            elif p.type == SOUTH_TO_WEST and elbow == SOUTH_TO_EAST:
+                crossed -= 2
+                elbow = ""
+            elif p.type == NORTH_TO_WEST and elbow == SOUTH_TO_EAST:
+                crossed -= 1
+                elbow = ""
+            elif p.type == SOUTH_TO_WEST and elbow == NORTH_TO_EAST:
+                crossed -= 1
+                elbow = ""
+            else:
+                elbow = p.type
+        return crossed
+        
     def __pipe(self, x: int, y: int) -> Pipe:
-        if x < 0 or x >= self.cols or y < 0 or y >= self.rows:
+        if x < 0 or x >= self.col_count or y < 0 or y >= self.row_count:
             return None
         pipe = self.pipes[y][x]
         if pipe == None:
@@ -99,109 +167,7 @@ class EdgeSearcher(Searcher):
         if not pipe.inloop:
             return None
         return pipe
-    
-    def possible_moves(self, obj) -> list[SearchMove]:
-        moves = set()
-        x, y = obj
-        for xo, yo in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            xo += x
-            yo += y
-            if xo == -1 or xo == self.cols or yo == -1 or yo == self.rows:
-                moves.add((xo, yo))
-            elif self.pipes[yo][xo] == None or not self.pipes[yo][xo].inloop:
-                moves.add((xo, yo))
-        
-        for left, right, yo, dir in [(x-1, x, -1, 'U'), (x, x+1, -1, 'U'), (x-1, x, 1, 'D'), (x, x+1, 1, 'D')]:
-            v = y + yo
-            lpipe = self.__pipe(left, v)
-            rpipe = self.__pipe(right, v)
-            
-            if lpipe == None or rpipe == None:
-                if lpipe == None:
-                    moves.add((left, v))
-                if rpipe == None:
-                    moves.add((right, v))
-            else:
-                if lpipe.pos not in rpipe.links:
-                    self.__squeeze(lpipe.pos, rpipe.pos, moves, dir, set())
-        
-        for top, bottom, xo, dir in [(y-1, y, -1, 'L'), (y, y+1, -1, 'L'), (y-1, y, 1, 'R'), (y, y+1, 1, 'R')]:
-            h = x + xo
-            tpipe = self.__pipe(h, top)
-            bpipe = self.__pipe(h, bottom)
-            
-            if tpipe == None or bpipe == None:
-                if tpipe == None:
-                    moves.add((h, top))
-                if bpipe == None:
-                    moves.add((h, bottom))
-            else:
-                if tpipe.pos not in bpipe.links:
-                    self.__squeeze(tpipe.pos, bpipe.pos, moves, dir, set())
-                
-        rmoves = []
-        for move in moves:
-            rmoves.append(SearchMove(1, move))
-        return rmoves
-    
-    def distance_from_goal(self, obj) -> int:
-        fromleft = obj[0]
-        fromright = self.cols - obj[0]
-        fromtop = obj[1]
-        frombottom = self.rows - obj[1]
-        m = min(fromleft, fromright, fromtop, frombottom)
-        if m < 0:
-            return 0
-        else:
-            return m + 1
-            
-    def __squeeze(self, pos1, pos2, moves, dir, searched):    
-        pos1x, pos1y = pos1
-        pos2x, pos2y = pos2
-        s = (pos1x, pos1y, pos2x, pos2y)
-        if s in searched:
-            return
-        searched.add(s)
-        
-        checks = []
-        if dir == 'L':
-            checks = [
-                ('L', pos1x-1, pos1y, pos2x-1, pos2y), 
-                ('D', pos2x-1, pos2y, pos2x, pos2y),
-                ('U', pos1x-1, pos1y, pos1x, pos1y), 
-            ]
-        elif dir == 'R':
-            checks = [
-                ('R', pos1x+1, pos1y, pos2x+1, pos2y), 
-                ('D', pos2x, pos2y, pos2x+1, pos2y),
-                ('U', pos1x, pos1y, pos1x+1, pos1y),
-            ]
-        elif dir == 'D':
-            checks = [
-                ('D', pos1x, pos1y+1, pos2x, pos2y+1), 
-                ('R', pos2x, pos2y, pos2x, pos2y+1), 
-                ('L', pos1x, pos1y, pos1x, pos1y+1),
-            ]
-        else:
-            checks = [
-                ('U', pos1x, pos1y-1, pos2x, pos2y-1), 
-                ('R', pos2x, pos2y+1, pos2x, pos2y), 
-                ('L', pos1x, pos1y+1, pos1x, pos1y),
-            ]
-        
-        for check in checks:
-            pos1pipe = self.__pipe(check[1], check[2])
-            pos2pipe = self.__pipe(check[3], check[4])
-            
-            if pos1pipe == None or pos2pipe == None:
-                if pos1pipe == None:
-                    moves.add((check[1], check[2]))
-                if pos2pipe == None:
-                    moves.add((check[3], check[4]))
-            else:
-                if pos1pipe.pos not in pos2pipe.links:
-                    self.__squeeze(pos1pipe.pos, pos2pipe.pos, moves, check[0], searched)
-    
+
 def input_to_pipes(lines: list[str]) -> (list[list[Pipe]], tuple):
     # parse pipes
     pipes = []
@@ -278,13 +244,13 @@ def navigate_pipe(pipes: list[list[Pipe]], start: tuple[int]) -> int:
     return steps
 
 # Part 1
-input = read_lines("input/day10-input.txt")
-sample = read_lines("input/day10-sample.txt")
-sample2 = read_lines("input/day10-sample2.txt")
-sample3 = read_lines("input/day10-sample3.txt")
-sample4 = read_lines("input/day10-sample4.txt")
-sample5 = read_lines("input/day10-sample5.txt")
-sample6 = read_lines("input/day10-sample6.txt")
+input = read_lines("input/day10/input.txt")
+sample = read_lines("input/day10/sample.txt")
+sample2 = read_lines("input/day10/sample2.txt")
+sample3 = read_lines("input/day10/sample3.txt")
+sample4 = read_lines("input/day10/sample4.txt")
+sample5 = read_lines("input/day10/sample5.txt")
+sample6 = read_lines("input/day10/sample6.txt")
 
 value = solve_part1(sample)
 assert(value == 4)
@@ -303,6 +269,4 @@ assert(value == 8)
 value = solve_part2(sample6)
 assert(value == 10)
 value = solve_part2(input)
-assert(value < 809)
-assert(value != 686)
-assert(value == -1)
+assert(value == 579)
