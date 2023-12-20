e415af9cd0564d90efb15f7d07afc3f0db320043
scottdharris <scottdharris11@yahoo.com>
2023-12-20 07:03:48 -0600

    day 20 part 1

--- day20.py
+++ day20.py
@@ -1,25 +1,166 @@
+import types
+from typing import Any
 from utilities.data import read_lines
 from utilities.runner import Runner
 
 @Runner("Day 20", "Part 1")
 def solve_part1(lines: list):
-    return -1
+    modules = create_modules(lines)
+    high_cnt = 0
+    low_cnt = 0
+    for _ in range(1000):
+        queue = [Pulse("button", "broadcaster", LOW_PULSE)]
+        while len(queue) != 0:
+            pulse = queue[0]
+            if pulse.ptype == HIGH_PULSE:
+                high_cnt += 1
+            else:
+                low_cnt += 1
+            queue.extend(modules[pulse.to_id].process_pulse(pulse))
+            queue.pop(0)
+    return high_cnt * low_cnt
 
 @Runner("Day 20", "Part 2")
 def solve_part2(lines: list):
-    return -1
+    modules = create_modules(lines)
+    print(modules["rx"])
+
+HIGH_PULSE = 1
+LOW_PULSE = 0
+
+class Pulse:
+    def __init__(self, from_id: str, to_id: str, ptype: int) -> None:
+        self.from_id = from_id
+        self.to_id = to_id
+        self.ptype = ptype
+        
+    def __repr__(self) -> str:
+        return str((self.from_id, self.to_id, self.ptype))
+        
+class Module:
+    def __init__(self, id: str) -> None:
+        self.id = id
+        self.inputs = []
+        self.dest = []
+        
+    def __repr__(self) -> str:
+        iids = []
+        for i in self.inputs:
+            iids.append(i.id)    
+        dids = []
+        for d in self.dest:
+            dids.append(d.id)
+        return ", ".join(iids) + " -> " + self.id + " -> " + ", ".join(dids)
+            
+    def add_destination(self, d: "Module") -> None:
+        self.dest.append(d)
+    
+    def add_input(self, i: "Module") -> None:
+        self.inputs.append(i)
+        
+    def send_pulse(self, ptype: int) -> list[Pulse]:
+        out = []
+        for d in self.dest:
+            out.append(Pulse(self.id, d.id, ptype))
+        return out
+    
+    def process_pulse(self, _: Pulse) -> list[Pulse]:
+        return []
+
+class FlipFlopModule(Module):
+    def __init__(self, id: str) -> None:
+        super().__init__(id)
+        self.on = False
+    
+    def __repr__(self) -> str:
+        return super().__repr__() + ": " + str(self.on)
+        
+    def process_pulse(self, pulse: Pulse) -> list[Pulse]:
+        if pulse.ptype == HIGH_PULSE:
+            return []
+        self.on = not self.on
+        send = LOW_PULSE
+        if self.on:
+            send = HIGH_PULSE
+        return super().send_pulse(send)
+    
+class ConjunctionModule(Module):
+    def __init__(self, id: str) -> None:
+        super().__init__(id)
+        self.last = {}
+        
+    def __repr__(self) -> str:
+        return super().__repr__() + ": " + str(self.last)
+    
+    def add_input(self, i: "Module") -> None:
+        self.last[i.id] = LOW_PULSE
+    
+    def process_pulse(self, pulse: Pulse) -> list[Pulse]:
+        self.last[pulse.from_id] = pulse.ptype
+        send = LOW_PULSE
+        for l in self.last.values():
+            if l == LOW_PULSE:
+                send = HIGH_PULSE
+                break
+        return super().send_pulse(send)
+
+class BroadcastModule(Module):
+    def process_pulse(self, pulse: Pulse) -> list[Pulse]:
+        return super().send_pulse(pulse.ptype)
+
+class OutputModule(Module):
+    pass
+
+class MoverModule(Module):
+    def __init__(self, id: str) -> None:
+        super().__init__(id)
+        self.on = False
+        
+    def process_pulse(self, pulse: Pulse) -> list[Pulse]:
+        if pulse.ptype == LOW_PULSE:
+            self.on = True
+        return []
+        
+def create_modules(lines: list[str]) -> dict[str,Module]:
+    modules = {}
+    for line in lines:
+        id = line.split()[0]
+        if id == "broadcaster":
+            modules[id] = BroadcastModule(id)
+        elif id[0] == "%":
+            modules[id[1:]] = FlipFlopModule(id[1:])
+        elif id[0] == "&":
+            modules[id[1:]] = ConjunctionModule(id[1:])
+    
+    for line in lines:
+        pieces = line.split()
+        id = pieces[0]
+        if id[0] == "%" or id[0] == "&":
+            id = id[1:]
+        for d in pieces[2:]:
+            d = d.replace(",", "")
+            if d not in modules:
+                if d == "rx":
+                    modules[d] = MoverModule(d)
+                else:
+                    modules[d] = OutputModule(d)
+            modules[id].add_destination(modules[d])
+            modules[d].add_input(modules[id])
+    
+    return modules
 
 # Part 1
 input = read_lines("input/day20/input.txt")
 sample = read_lines("input/day20/sample.txt")
+sample2 = read_lines("input/day20/sample2.txt")
 
 value = solve_part1(sample)
-assert(value == -1)
+assert(value == 32000000)
+value = solve_part1(sample2)
+assert(value == 11687500)
 value = solve_part1(input)
-assert(value == -1)
+assert(value == 818723272)
 
 # Part 2
-value = solve_part2(sample)
-assert(value == -1)
 value = solve_part2(input)
 assert(value == -1)
