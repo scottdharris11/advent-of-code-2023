83621537b35bcea971339f5175a5fb58dfaa2edf
scottdharris <scottdharris11@yahoo.com>
2023-12-21 07:06:49 -0600

    day 21 part 2 rules correct but can't finish

--- day21.py
+++ day21.py
@@ -14,8 +14,16 @@ def solve_part1(lines: list, steps: int):
     return len(before)
 
 @Runner("Day 21", "Part 2")
-def solve_part2(lines: list):
-    return -1
+def solve_part2(lines: list, steps: int):
+    m = Map(lines)
+    before = [m.start]
+    for _ in range(steps):
+        after = set()
+        for b in before:
+            after.update(m.moves_from(b))
+        before.clear()
+        before.extend(after)
+    return len(before)
 
 class Map:
     def __init__(self, lines: list) -> None:
@@ -25,7 +33,7 @@ class Map:
         for y, row in enumerate(self.grid):
             for x, col in enumerate(row):
                 if col == "S":
-                    self.start = (x, y)
+                    self.start = (x, y, 0, 0)
                     return
     
     def moves_from(self, current: tuple[int]) -> list[tuple[int]]:
@@ -33,11 +41,25 @@ class Map:
         for move in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             nx = current[0] + move[0]
             ny = current[1] + move[1]
-            if nx < 0 or nx == self.col_count or ny < 0 or ny == self.row_count:
-                continue
+            mx = current[2]
+            my = current[3]
+            
+            if nx < 0:
+                mx -= 1
+                nx = self.col_count - 1
+            elif nx == self.col_count:
+                mx += 1
+                nx = 0
+            if ny < 0:
+                my -= 1
+                ny = self.row_count - 1
+            elif ny == self.row_count:
+                my += 1
+                ny = 0
+            
             if self.grid[ny][nx] == "#":
                 continue
-            moves.append((ny, nx))
+            moves.append((nx, ny, mx, my))
         return moves
 
 # Part 1
@@ -50,7 +72,19 @@ value = solve_part1(input, 64)
 assert(value == 3820)
 
 # Part 2
-value = solve_part2(sample)
-assert(value == -1)
-value = solve_part2(input)
+value = solve_part2(sample, 6)
+assert(value == 16)
+value = solve_part2(sample, 10)
+assert(value == 50)
+value = solve_part2(sample, 50)
+assert(value == 1594)
+value = solve_part2(sample, 100)
+assert(value == 6536)
+value = solve_part2(sample, 500)
+assert(value == 167004)
+value = solve_part2(sample, 1000)
+assert(value == 668697)
+value = solve_part2(sample, 5000)
+assert(value == 16733044)
+value = solve_part2(input, 26501365)
 assert(value == -1)
