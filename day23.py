91dd3b2bf644738b54e2f343e0843d4d4fed1a6c
scottdharris <scottdharris11@yahoo.com>
2023-12-23 01:14:42 -0600

    perf tweak and part 2 code, wrong and slow

--- day23.py
+++ day23.py
@@ -3,7 +3,62 @@ from utilities.runner import Runner
 
 @Runner("Day 23", "Part 1")
 def solve_part1(lines: list):
-    island = Island(lines)
+    island = Island(lines, True)
+    return max_hike(island)
+
+@Runner("Day 23", "Part 2")
+def solve_part2(lines: list):
+    island = Island(lines, False)
+    return max_hike(island)
+
+class Island:
+    def __init__(self, lines: list[str], onlydown: bool) -> None:
+        self.grid = lines
+        self.row_count = len(lines)
+        self.col_count = len(lines[0])
+        self.start = (1, 0)
+        self.onlydown = onlydown
+        self.moves_cache = {}
+        
+    def moves_from(self, current: tuple[int]) -> list[tuple[int]]:
+        if current in self.moves_cache:
+            return self.moves_cache[current]
+        moves = []
+        for move in [(1,0,">"), (-1,0,"<"), (0,1,"v"), (0,-1,"^")]:
+            x = move[0] + current[0]
+            y = move[1] + current[1]
+            next = self.grid[y][x]
+            if next == "#":
+                continue
+            if self.onlydown and (next != "." and next != move[2]):
+                continue
+            moves.append((x, y))
+        self.moves_cache[current] = moves
+        return moves
+    
+    def path_end(self, current: tuple[int]) -> bool:
+        return current[1] == self.row_count - 1
+
+class Hike:
+    def __init__(self, steps: list[tuple[int]]) -> None:
+        self.steps = steps
+        self.ss = set(steps)
+        self.done = False
+        self.goal = False
+        self.current = steps[-1]
+    
+    def copy(self) -> "Hike":
+        return Hike(self.steps[:])
+        
+    def already_been(self, loc: tuple[int]) -> bool:
+        return loc in self.ss
+    
+    def step(self, loc: tuple[int]) -> None:
+        self.steps.append(loc)
+        self.ss.add(loc)
+        self.current = loc
+
+def max_hike(island: Island) -> int:
     hikes = [Hike([island.start])]
     alldone = False
     while not alldone:
@@ -19,47 +74,6 @@ def solve_part1(lines: list):
     for hike in hikes:
         steps.append(len(hike.steps)-1)
     return max(steps)
-
-@Runner("Day 23", "Part 2")
-def solve_part2(lines: list):
-    return -1
-
-class Island:
-    def __init__(self, lines: list[str]) -> None:
-        self.grid = lines
-        self.row_count = len(lines)
-        self.col_count = len(lines[0])
-        self.start = (1, 0)
-        
-    def moves_from(self, current: tuple[int]) -> list[tuple[int]]:
-        moves = []
-        for move in [(1,0,">"), (-1,0,"<"), (0,1,"v"), (0,-1,"^")]:
-            x = move[0] + current[0]
-            y = move[1] + current[1]
-            next = self.grid[y][x]
-            if next == "." or next == move[2]:
-                moves.append((x, y))
-        return moves
-    
-    def path_end(self, current: tuple[int]) -> bool:
-        return current[1] == self.row_count - 1
-
-class Hike:
-    def __init__(self, steps: list[tuple[int]]) -> None:
-        self.steps = steps
-        self.done = False
-        self.goal = False
-        self.current = steps[-1]
-    
-    def copy(self) -> "Hike":
-        return Hike(self.steps[:])
-        
-    def already_been(self, loc: tuple[int]) -> bool:
-        return loc in self.steps
-    
-    def step(self, loc: tuple[int]) -> None:
-        self.steps.append(loc)
-        self.current = loc
 
 def take_hike(island: Island, hike: Hike) -> list[Hike]:
     altpaths = []
@@ -98,6 +112,6 @@ assert(value == 2030)
 
 # Part 2
 value = solve_part2(sample)
-assert(value == -1)
+assert(value == 154)
 value = solve_part2(input)
 assert(value == -1)
