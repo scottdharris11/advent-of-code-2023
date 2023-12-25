635d12718e9df560b5b458de599ac5f4ae15b2e1
scottdharris <scottdharris11@yahoo.com>
2023-12-24 22:37:32 -0600

    optimize attempt

--- day21.py
+++ day21.py
@@ -1,3 +1,4 @@
+import cProfile
 from utilities.data import read_lines
 from utilities.runner import Runner
 
@@ -22,30 +23,34 @@ class Map:
         self.col_count = len(lines[0])
         self.move_cache = {}
         self.tiles = {}
+        self.max = 0
         for y, row in enumerate(self.grid):
             for x, col in enumerate(row):
                 if col == "S":
-                    self.tiles[(0,0)] = set()
-                    self.tiles[(0,0)].add((x, y))
-                    return
+                    self.tiles[(0,0)] = MapTile((0,0), (x, y))
+                if col != "#":
+                    self.max += 1
     
     def step(self) -> None:
-        ntiles = {}
         lasttilekey = None
         lasttile = None
-        for tile in self.tiles.keys():
-            for location in self.tiles[tile]:
+        for key, tile in self.tiles.copy().items():
+            if tile.maxed:
+                continue
+            for location in tile.locations():
                 if location not in self.move_cache:
                     self.move_cache[location] = self.moves(location)
                 moves = self.move_cache[location]
                 for move in moves:
-                    tilekey = (tile[0]+move[2], tile[1]+move[3])
+                    tilekey = (key[0]+move[2], key[1]+move[3])
                     if lasttile == None or lasttilekey != tilekey:
-                        lasttile = ntiles.get(tilekey, set())
+                        if tilekey not in self.tiles:
+                            self.tiles[tilekey] = MapTile(tilekey, None)
+                        lasttile = self.tiles[tilekey]
                         lasttilekey = tilekey
-                    lasttile.add((move[0], move[1]))
-                    ntiles[tilekey] = lasttile
-        self.tiles = ntiles
+                    lasttile.add_step((move[0], move[1]))
+        for tile in self.tiles.values():
+            tile.complete_cycle(self.max)
     
     def moves(self, location: tuple[int]) -> list[tuple[int]]:
         moves = []
@@ -77,8 +82,52 @@ class Map:
     def possible_locations(self) -> int:
         total = 0
         for tile in self.tiles.values():
-            total += len(tile)
+            total += tile.plots_reachable()
         return total
+
+class MapTile:
+    def __init__(self, sliver: tuple[int], start: tuple[int]) -> None:
+        self.sliver = sliver
+        self.plots = set()
+        if start != None:
+            self.plots.add(start)
+        self.stage = set()
+        self.maxed = False
+        self.prev = 0
+        self.last = 0
+        self.maxcnt = 0
+        
+    def add_step(self, step: tuple[int]) -> None:
+        if self.maxed:
+            return
+        self.stage.add(step)
+        
+    def complete_cycle(self, max: int) -> None:
+        if self.maxed:
+            t = self.prev
+            self.prev = self.last
+            self.last = t
+            return
+        
+        self.prev = self.last
+        self.last = len(self.stage)
+        if self.last > max/2:
+            self.maxcnt += 1
+        if self.maxcnt >= 10:
+            self.maxed = True
+            self.plots = None
+            self.stage = None
+        else:
+            self.plots = self.stage
+            self.stage = set()
+        
+    def locations(self) -> set[tuple[int]]:
+        if self.maxed:
+            return []
+        return self.plots
+    
+    def plots_reachable(self) -> int:
+        return self.last
 
 # Part 1
 input = read_lines("input/day21/input.txt")
@@ -100,9 +149,9 @@ value = solve_part2(sample, 100)
 assert(value == 6536)
 value = solve_part2(sample, 500)
 assert(value == 167004)
-#value = solve_part2(sample, 1000)
-#assert(value == 668697)
-#value = solve_part2(sample, 5000)
-#assert(value == 16733044)
+value = solve_part2(sample, 1000)
+assert(value == 668697)
+value = solve_part2(sample, 5000)
+assert(value == 16733044)
 #value = solve_part2(input, 26501365)
 #assert(value == -1)
